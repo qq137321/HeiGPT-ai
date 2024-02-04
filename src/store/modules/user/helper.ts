c1943ca84d9b2bc2f2f11229badd5b8761cd8de4
SphinxHill <137321@qq.com>
2024-02-05 04:55:58 +0800

    up1

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -17,7 +17,7 @@ export function defaultSetting(): UserState {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Dooy/chatgpt-web-midjourney-proxy/main/src/assets/avatar.jpg',
       name:  t('mjset.sysname'),//'AI绘图',
-      description: 'CopyRight <a href="https://github.com/Dooy/chatgpt-web-midjourney-proxy" class="text-blue-500" target="_blank" >HeiGPT</a>',
+      description: 'CopyRight <a href="https://HeiGPT.com/" class="text-blue-500" target="_blank" >HeiGPT</a>',
     },
   }
 }
