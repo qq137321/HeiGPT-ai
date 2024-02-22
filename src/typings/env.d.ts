e94cfdef1994997f3f8cd9b9af1d9a2de1bed5b8
qq137321 <137321@qq.com>
2024-02-22 17:45:41 +0800

    uploads file

--- src/typings/env.d.ts
+++ src/typings/env.d.ts
@@ -5,4 +5,14 @@ interface ImportMetaEnv {
 	readonly VITE_APP_API_BASE_URL: string;
 	readonly VITE_GLOB_OPEN_LONG_REPLY: string;
 	readonly VITE_GLOB_APP_PWA: string;
+	
+	readonly API_UPLOADER: string;
+	readonly FILE_SERVER: string;
+	readonly FILE_SERVER_API_KEY: string;
+	readonly R2_DOMAIN: string;
+	readonly R2_BUCKET_NAME: string;
+	readonly R2_ACCOUNT_ID: string;
+	readonly R2_KEY_ID: string;
+	readonly R2_KEY_SECRET: string;
+
 }
