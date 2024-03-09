2ac864313edbad8b21d859d2eed5ad271f59a447
qq137321 <129837293+qq137321@users.noreply.github.com>
2024-03-10 00:41:10 +0800

    up

--- src/api/openapi.ts
+++ src/api/openapi.ts
@@ -103,10 +103,12 @@ export const GptUploader =   ( url:string, FormData:FormData )=>{
     //
 
     // 直接给它写死，不要其他上传了。不会引入环境变量，所以直接设置死
-    url= gptServerStore.myData.UPLOADER_URL? gptServerStore.myData.UPLOADER_URL :  'https://one-api.bltcy.top/v1/files'
+    // url= gptServerStore.myData.UPLOADER_URL? gptServerStore.myData.UPLOADER_URL :  'https://one-api.bltcy.top/v1/files'
+    url= gptServerStore.myData.UPLOADER_URL? gptServerStore.myData.UPLOADER_URL :  'http://api.gptgod.online/v1/file'
     let headers=   {'Content-Type': 'multipart/form-data',
-    'Authorization': 'Bearer sk-9RBUaBgg5QZxiQvCEcD80a887d1e4bFfBfA884603fC9B975'
-     }
+    // 'Authorization': 'Bearer sk-9RBUaBgg5QZxiQvCEcD80a887d1e4bFfBfA884603fC9B975'
+    'Authorization': 'Bearer sk-EUqEDfom7OQmW01vPnCZseO2920iWs5XzTBYVytjAkRGev43'
+         }
 
     // one-api.bltcy.top返回数据中包括文件大小字样，可以用来扣费用
     return new Promise<any>((resolve, reject) => {
@@ -175,7 +177,8 @@ function getUploadFileHeaderAuthorization(){
         return {}
     }
     return {
-        'Authorization': 'Bearer sk-9RBUaBgg5QZxiQvCEcD80a887d1e4bFfBfA884603fC9B975'
+        //'Authorization': 'Bearer sk-9RBUaBgg5QZxiQvCEcD80a887d1e4bFfBfA884603fC9B975'
+        'Authorization': 'Bearer sk-EUqEDfom7OQmW01vPnCZseO2920iWs5XzTBYVytjAkRGev43'
     }
 }
 
