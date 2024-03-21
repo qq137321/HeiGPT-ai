d6aec1a61a041505c55eaa0f2178ae781c5d7834
qq137321 <129837293+qq137321@users.noreply.github.com>
2024-03-22 00:01:15 +0800

    up

--- src/views/mj/aiGpt.vue
+++ src/views/mj/aiGpt.vue
@@ -326,7 +326,8 @@ let emsgnotype = JSON.stringify(jsonEmgs, null, 2);
                     ,uuid:st.value.uuid //当前会话
                     ,onMessage:(d)=>{
                         mlog('🐞消息',d);
-                        textRz.value.push(d.text + "1up1");
+                        textRz.value.push(d.text);
+                        // textRz.value.push(d.text + "1up1");
                     }
                     ,onError:(e:any)=>{
                         mlog('onError',e)
