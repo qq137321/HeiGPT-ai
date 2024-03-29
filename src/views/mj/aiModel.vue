1986c8e69362622400e8bf5d0a687079c9aa3238
qq137321 <129837293+qq137321@users.noreply.github.com>
2024-03-30 01:53:04 +0800

    up

--- src/views/mj/aiModel.vue
+++ src/views/mj/aiModel.vue
@@ -16,8 +16,7 @@ const nGptStore = ref(  chatSet.getGptConfig() );
 const config = ref({
 model:[ 'gpt-3.5-turbo',`gpt-3.5-turbo-16k`,'gpt-4','gpt-4-32k','gpt-4-turbo-preview','gpt-4-1106-preview','gpt-4-0125-preview'
 ,`gpt-4-vision-preview`,'gpt-4-all','bing','suno-v2','suno-v3','google-gemini-pro'
-,'claude-2.1','claude-3-haiku-20240307'
-,'claude-3-opus-20240229','claude-3-sonnet-20240229'
+,'claude-3-haiku-20240307','claude-3-opus-20240229','claude-3-sonnet-20240229'
 ,'llama-2-13b','llama-2-70b','Code Interpreter'
 ,'dall-e-3','stable-diffusion','midjourney','midjourney-fast','midjourney-relax'
 ]
