a40515d0711fdc270596ea531f4c718ebc57a801
qq137321 <129837293+qq137321@users.noreply.github.com>
2024-04-15 02:46:50 +0800

    up

--- src/views/mj/aiModel.vue
+++ src/views/mj/aiModel.vue
@@ -15,9 +15,9 @@ const nGptStore = ref(  chatSet.getGptConfig() );
 
 const config = ref({
 model:[ 'gpt-3.5-turbo',`gpt-3.5-turbo-16k`,'gpt-4','gpt-4-32k','gpt-4-turbo-preview','gpt-4-turbo-2024-04-09','gpt-4-1106-preview','gpt-4-0125-preview'
-,`gpt-4-vision-preview`,'gpt-4-all','bing','suno-v2','suno-v3','gemini-pro-1.5'
+,`gpt-4-vision-preview`,'gpt-4-all','bing-creative','bing-balanced','bing-recise','bing-gpt-4','suno-v2','suno-v3','gemini-pro-1.5'
 ,'claude-3-haiku-20240307','claude-3-opus-20240229','claude-3-sonnet-20240229'
-,'dall-e-3','stable-diffusion','llama-2-13b','llama-2-70b','Code Interpreter'
+,'dall-e-3','stable-diffusion','llama-2-70b','Code Interpreter'
 ]
 ,maxToken:2048
 });
